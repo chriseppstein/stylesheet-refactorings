81f30068b036a8e1a427af68242e5abc3c26d47a
Chris Eppstein <chris@eppsteins.net>
2010-10-28 22:47:22 -0700

    Initial project setup. No changes yet.

---
+++ config.rb
@@ -0,0 +1,7 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "output"
+sass_dir = "sass"
+output_style = :compact
+line_comments = false
